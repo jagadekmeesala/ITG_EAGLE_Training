28c5abdc1c44d15c9c8859b62c78bfd325c2ac7e
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 16:39:20 +0530

    Create Three.java

---
+++ Three.java
@@ -0,0 +1,10 @@
+public class Divisible {
+    public static void main(String[] args) {
+        int num = 30;
+        if ((num % 5 == 0) && (num % 3 == 0)) {
+            System.out.println("Divisible by 3 and 5 ");
+        } else {
+            System.out.println("Not divisible by 3 and 5");
+        }
+    }
+}
