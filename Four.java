cf203e008ee1ea82fa22b46e91b9c22ddf6bea69
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 12:13:50 +0530

    Create Four.java

---
+++ Four.java
@@ -0,0 +1,16 @@
+public class Four
+{
+	public static void main(String[] args) {
+	    double num1 = 100;
+	    double num2 = 200;
+		double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+	}
+}
