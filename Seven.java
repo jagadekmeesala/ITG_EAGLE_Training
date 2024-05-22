306a0656f9e1baafeb1f5543764e538f0629523a
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 17:40:35 +0530

    Create Seven.java

---
+++ Seven.java
@@ -0,0 +1,9 @@
+public class Square{
+    public static void main(String args[]){
+        int n = 20;
+        for ( int i = 0; i<= n; i++){
+            int sq = i * i;
+            System.out.println( sq );
+        }
+    }
+}
