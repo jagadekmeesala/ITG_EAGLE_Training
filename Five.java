f5ece09ce477708af5b4517a827b05ccdfed7507
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 12:21:58 +0530

    Create Five.java

---
+++ Five.java
@@ -0,0 +1,12 @@
+public class Five
+{
+	public static void main(String[] args) {
+	  double num1 = 10.5;
+    double num2 = 20.7;
+    double num3 = 30.2;
+    double sum = num1 + num2 + num3;
+    double average = sum / 3;
+    System.out.println(sum);
+    System.out.println(average);
+	}
+}
