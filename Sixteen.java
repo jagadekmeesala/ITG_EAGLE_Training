851ae712c7d3cc1653f709da45a8390b85246e63
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 16:14:00 +0530

    Create Sixteen.java

---
+++ Sixteen.java
@@ -0,0 +1,13 @@
+public class MethodDemo {
+    static void staticMethod() {
+        System.out.println("This is a static method.");
+    }
+    void nonStaticMethod() {
+        System.out.println("This is a non-static method.");
+    }
+    public static void main(String[] args) {
+        staticMethod();   
+        MethodDemo obj = new MethodDemo();
+        obj.nonStaticMethod();
+    }
+}
