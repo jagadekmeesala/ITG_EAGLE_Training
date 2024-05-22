295a3f12d0f5a6fb54b98b0693160dcb819a0301
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 17:34:23 +0530

    Create Six.java

---
+++ Six.java
@@ -0,0 +1,11 @@
+public class MinMax{
+    public static void main(String args[]){
+        int a = 20;
+        int b = 30;
+        if (a>b){
+            System.out.println( "A " + a + " is bigger than B " + b);
+        } else{
+            System.out.println( "B " + b + " is bigger than " + a);
+        }
+    }
+}
