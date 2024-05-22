4b5b509cf807dd0fccd11e7886595f5f72f7d710
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 22:28:28 +0530

    Create ThirtyThree.java

---
+++ ThirtyThree.java
@@ -0,0 +1,11 @@
+public class NaturalSum {
+    public static void main(String args[]) {
+      int n = 1078;
+      for (int i =0; i<= n; i++){
+          int sum = (n*(n+1)/2);
+          
+        System.out.println(sum);
+        break;
+      }
+    }
+}
