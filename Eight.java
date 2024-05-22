12f5831878c5b85632c2e2447b0fc11f2f014137
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-22 16:51:20 +0530

    Create Eight.java

---
+++ Eight.java
@@ -0,0 +1,11 @@
+public class Even {
+    public static void main(String[] args) {
+        int N = 100; 
+        int sum = 0; 
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        } 
+    }
+}
