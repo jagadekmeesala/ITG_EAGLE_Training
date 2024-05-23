ac8de2e7448ca09f8ad574aaf86954b7b68567c5
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-23 14:00:19 +0530

    Create TwentyThree.java

---
+++ TwentyThree.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 12345;
+        int reverse = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+        System.out.println(reverse);
+    }
+}
