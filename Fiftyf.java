70fe2cba747e14e3609d7361860f27678a6797bc
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-23 18:49:18 +0530

    Create Fiftyf.java

---
+++ Fiftyf.java
@@ -0,0 +1,12 @@
+public class Pattern{
+    public static void main(String args[]){
+        int c = 1;
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c++ + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
