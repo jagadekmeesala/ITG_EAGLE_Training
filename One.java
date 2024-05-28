615352162e41243f952db38441d3d6fd8943e4ca
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-28 19:35:05 +0530

    Create One.java

---
+++ One.java
@@ -0,0 +1,9 @@
+public class CharArray{
+     public static void main(String []args){
+         String s = "MVVSDJAGADEK'1234";
+         char[] A = s.toCharArray();
+         for(char c: A){
+             System.out.println(c+" ");
+         }
+         }
+     }
