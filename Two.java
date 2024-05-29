720526cc836475c1f6eeb9a86e79cd2d70fcb531
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-05-29 15:28:38 +0530

    Update Two.java

--- Two.java
+++ Two.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     String s = "Hello World!";
     int length = 0;
-    for (char c : s.toCharArray()) {
+    for (char charArray : s.toCharArray()) {
       length++; 
     }
     
