1bcb7fc4af670ce38fa3b3611ee4c4e8d04bcf5a
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-06-01 17:32:04 +0530

    Create Two.java

---
+++ Two.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+public class FinBi {
+
+    public static void main(String[] args) throws IOException {
+        
+        // FileInputStream ip = new FileInputStream("/home/miracle/Desktop/am.ml");
+        // int i;
+        // while ((i = ip.read()) != -1) {
+        //     System.out.print((char) i);
+        // }
+        // System.out.println("\nRead Successfully using FIS");
+        // ip.close(); 
+
+        // FileInputStream ip = new FileInputStream("/home/miracle/Desktop/am.ml");
+        // BufferedInputStream bip = new BufferedInputStream(ip);
+        // int i;
+        // while((i=bip.read())!= -1){
+        //     System.out.print((char) i);
+        // }
+        // System.out.println("Read Successfully using BIS");
+        // bip.close();
+
+    }
+}
