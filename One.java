cc7fd5fa23f9c9aa8234a1014e8117191790bbe0
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-06-01 17:27:02 +0530

    Create One.java

---
+++ One.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class FonBo {
+
+    public static void main(String[] args) throws IOException {
+        
+        // FileOutputStream op = new FileOutputStream("/home/miracle/Desktop/output.txt");
+        // String dataToWrite = "Now I am going to write the data using FO stream";
+        // byte[] bytesToWrite = dataToWrite.getBytes();
+        // op.write(bytesToWrite);
+        // op.close();
+        // System.out.println("Written Successfully using FOS");
+
+
+        // FileOutputStream op = new FileOutputStream("/home/miracle/Desktop/output.txt");
+        // BufferedOutputStream bos = new BufferedOutputStream(op);
+        // String dataToWrite = "Now I am going to write the data using BO stream";
+        // byte[] bytesToWrite = dataToWrite.getBytes();
+        // bos.write(bytesToWrite);
+        // bos.close();
+        // System.out.println("Written Successfully using BOS");
+
+    }
+}
