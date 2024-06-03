22f598c65d96974e70f09fd095d67fad944833a9
JAGADEK V.V.S.D. MEESALA <85881386+jagadekmeesala@users.noreply.github.com>
2024-06-03 10:41:02 +0530

    Create Five.java

---
+++ Five.java
@@ -0,0 +1,34 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Readfiles {
+public static void main(String[] args) throws FileNotFoundException, IOException{
+// FileInputStream fis = new FileInputStream("/home/miracle/Desktop/am.ml");
+// Scanner scanner = new Scanner(fis);
+// while (scanner.hasNextLine()) {
+// String line = scanner.nextLine();
+// System.out.println(line);
+// }
+// scanner.close();
+// fis.close();
+// System.out.println("Successfully read using FIS and  Scanner");
+
+    // FileInputStream ip = new FileInputStream("/home/miracle/Desktop/am.ml");
+    //         int i;
+    //         while ((i = ip.read()) != -1) {
+    //             System.out.print((char) i);
+    //         }
+    //         System.out.println("\nRead Successfully using FIS");
+    //         ip.close(); 
+
+        // FileInputStream ip = new FileInputStream("/home/miracle/Desktop/am.ml");
+        // BufferedInputStream bip = new BufferedInputStream(ip);
+        // int i;
+        // while((i=bip.read())!= -1){
+        //     System.out.print((char) i);
+        // }
+        // System.out.println("Read Successfully using BIS");
+        // bip.close();
+
+}
+}
